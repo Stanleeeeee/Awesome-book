f1fe747b04ae4aa9d97064b45ee0399d2e697e1e
yosefHesham <yousefmobdev@gmail.com>
2022-04-25 15:39:14 +0200

    Remove temp element from being rendered

--- books.js
+++ books.js
@@ -1,4 +1,5 @@
 const form = document.querySelector("form");
+const booksSections = document.querySelector(".books");
 
 let books = [
   {
@@ -9,17 +10,38 @@ let books = [
 ];
 
 localStorage.setItem("myname", "yousef");
+const renderBooks = () => {
+  console.log(booksSections);
+  console.log("Rendering");
+  for (let i = 0; i < books.length; i++) {
+    const currentBook = books[i];
+    const bookTemplate = document.createElement("bookTemp");
+    bookTemplate.innerHTML = `<div class="items-container">
+      <h2>${currentBook.title}</h2>
+      <p>${currentBook.author}</p>
+      <button type="button" class="btn" data="${currentBook.title}">Remove</button>
+      <hr class="line" /> </div>`;
 
-const fetchBooks = () => {
+    booksSections.appendChild(bookTemplate.firstChild);
+  }
+};
+
+const fetchAndRenderBooks = () => {
+  console.log("fetching");
   if (localStorage.getItem("books")) {
+    console.log("books already hre !");
     books = JSON.parse(localStorage.getItem("books"));
+    console.log(books);
   } else {
+    console.log("correct !");
     localStorage.setItem("books", JSON.stringify(books));
   }
+  renderBooks();
 };
 const addBook = (title, author) => {
   books.push({ title, author });
   localStorage.setItem("books", JSON.stringify(books));
+  renderBooks();
 };
 
 const removeBook = (title, author) => {
@@ -27,11 +49,17 @@ const removeBook = (title, author) => {
   localStorage.setItem("books", JSON.stringify(books));
 };
 
-form.addEventListener("input", () => {
+form.addEventListener("submit", () => {
   const title = form.elements.name.title;
   const author = form.elements.name.author;
 
   addBook(title, author);
 });
 
-window.onload = fetchBooks();
+// // const removeButton = document.querySelector(".btn");
+// removeButton.addEventListener("click", (e) => {
+//   removeButton.getAttribute("data");
+//   const bookTitle = removeButton.getAttribute("data");
+//   removeBook(bookTitle);
+// });
+window.onload = fetchAndRenderBooks();
