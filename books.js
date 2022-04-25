472ceffe42e06d47405388e391f9527214dcefa0
yosefHesham <yousefmobdev@gmail.com>
2022-04-25 17:48:22 +0200

    Add feedback when list is empty

--- books.js
+++ books.js
@@ -1,19 +1,22 @@
-const form = document.querySelector('form');
-const booksSections = document.querySelector('.books');
-const submitButton = document.querySelector('.submit-button');
+const form = document.querySelector("form");
+const booksSections = document.querySelector(".books");
+const submitButton = document.querySelector(".submit-button");
 
 let books = [
   {
-    title: 'Testeroo Testyy',
-    author: 'Lorem Ipsum',
+    title: "Testeroo Testyy",
+    author: "Lorem Ipsum",
   },
-  { title: 'Second Book', author: 'Testeroo Testyy' },
+  { title: "Second Book", author: "Testeroo Testyy" },
 ];
 
 const renderBooks = () => {
+  if (books.length === 0) {
+    booksSections.innerHTML = `<p>You dont have books yet, add some !</p>`;
+  }
   for (let i = 0; i < books.length; i += 1) {
     const currentBook = books[i];
-    const bookTemplate = document.createElement('bookTemp');
+    const bookTemplate = document.createElement("bookTemp");
     bookTemplate.innerHTML = `<div class="items-container">
       <h2>${currentBook.title}</h2>
       <p>${currentBook.author}</p>
@@ -25,26 +28,26 @@ const renderBooks = () => {
 };
 
 const fetchAndRenderBooks = () => {
-  if (localStorage.getItem('books')) {
-    books = JSON.parse(localStorage.getItem('books'));
+  if (localStorage.getItem("books")) {
+    books = JSON.parse(localStorage.getItem("books"));
   } else {
-    localStorage.setItem('books', JSON.stringify(books));
+    localStorage.setItem("books", JSON.stringify(books));
   }
   renderBooks();
 };
 const addBook = (title, author) => {
   books.push({ title, author });
-  localStorage.setItem('books', JSON.stringify(books));
+  localStorage.setItem("books", JSON.stringify(books));
   window.location.reload();
 };
 
 const removeBook = (title) => {
   books = books.filter((book) => book.title !== title);
-  localStorage.setItem('books', JSON.stringify(books));
+  localStorage.setItem("books", JSON.stringify(books));
   window.location.reload();
 };
 
-submitButton.addEventListener('click', (e) => {
+submitButton.addEventListener("click", (e) => {
   const title = form.elements.title.value;
   const author = form.elements.author.value;
   if (!title || !author) {
@@ -56,8 +59,10 @@ submitButton.addEventListener('click', (e) => {
 
 window.onload = fetchAndRenderBooks();
 
-const removeButton = document.querySelectorAll('.btn');
-removeButton.forEach((e) => e.addEventListener('click', () => {
-  const bookTitle = e.classList[1];
-  removeBook(bookTitle);
-}));
+const removeButton = document.querySelectorAll(".btn");
+removeButton.forEach((e) =>
+  e.addEventListener("click", () => {
+    const bookTitle = e.classList[1];
+    removeBook(bookTitle);
+  })
+);
