b2acea844458c8bcb04e4aaf5b55a9784db1f085
yosefHesham <yousefmobdev@gmail.com>
2022-04-27 14:45:15 +0200

    Resolve dependency cycle

--- js/book_collection.js
+++ js/book_collection.js
@@ -1,26 +1,26 @@
-import booksUi from "../books.js";
-import emptyList from "./custom_event.js";
+import emptyList from './custom_event.js';
+import booksSections from './booksui.js';
 
 class Books {
-  static books = JSON.parse(localStorage.getItem("books")) ?? [
+  static books = JSON.parse(localStorage.getItem('books')) ?? [
     {
-      title: "Testeroo Testyy",
-      author: "Lorem Ipsum",
+      title: 'Testeroo Testyy',
+      author: 'Lorem Ipsum',
     },
-    { title: "Second Book", author: "Testeroo Testyy" },
+    { title: 'Second Book', author: 'Testeroo Testyy' },
   ];
 
   static addBook = (title, author) => {
     this.books.push({ title, author });
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
   };
 
   static removeBook = (title, e) => {
     if (this.books.length === 1) {
-      booksUi.booksSections.dispatchEvent(emptyList);
+      booksSections.dispatchEvent(emptyList);
     }
     this.books = this.books.filter((book) => book.title !== title);
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
 
     e.target.parentElement.remove();
   };
